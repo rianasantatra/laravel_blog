336e845cadc7760a125a9788e31b7414a53e1eb8
rianasantatra <rianasantatra665@gmail.com>
2024-01-28 21:22:51 +0300

    local doc commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        require("@tailwindcss/forms")({
+            strategy: "class",
+        }),
+    ],
+};
