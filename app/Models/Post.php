4953b15079428ce1c9eddc9e498e532f4fcd5d9a
rianasantatra <rianasantatra665@gmail.com>
2024-07-21 22:08:57 +0300

    refactoring pour les filtrages par tags / catégories / formulaire de recherche

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -19,6 +20,29 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function scopeFilters(Builder $query, array $filters): void
+    {
+        if (isset($filters['search'])) {
+            $query->where(fn(Builder $query) => $query->where(
+                'title', 'LIKE', '%' . $filters['search'] . '%'
+            )->orWhere(
+                'content', 'LIKE', '%' . $filters['search'] . '%'
+            ));
+        }
+
+        if (isset($filters['category'])) {
+            $query->where(
+                'category_id', $filters['category']->id ?? $filters['category']
+            );
+        }
+
+        if (isset($filters['tag'])) {
+            $query->whereRelation(
+                'tags', 'tags.id', $filters['tag']->id ?? $filters['tag']
+            );
+        }
     }
 
     public function category(): BelongsTo
