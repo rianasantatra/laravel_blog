4a00166a140f7a861d8cc18ef86b633d3d99826a
rianasantatra <rianasantatra665@gmail.com>
2024-07-26 18:22:11 +0300

    finalisation formulaire inscription

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,6 @@
 <x-auth-layout title="Inscription" submitMessage="Inscription" :action='route("register")'>
     <x-input name="name" label="Nom Complet" />
+    <x-input name="email" label="Adresse Email" type="email" />
+    <x-input name="password" label="Mot de passe" type="password" />
+    <x-input name="password_confirmation" label="Confirmation mot de passe" type="password" />
 </x-auth-layout>
